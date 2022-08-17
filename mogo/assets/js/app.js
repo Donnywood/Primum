31923e40436114f6c46ea3a24d8b2c366f79338c
Donnywood <78824328+Donnywood@users.noreply.github.com>
2022-08-17 12:36:21 +0200

    Add files via upload

--- mogo/assets/js/app.js
+++ mogo/assets/js/app.js
@@ -29,15 +29,51 @@ $(function() {
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
         
-        var blockId = $(this).data('scroll'),
+        var $this = $(this),
+            blockId = $(this).data('scroll'),
             blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");    
         
 
         $("html, body").animate({
             scrollTop: blockOffset
         }, 500);    
+      
+    });
 
-          
+    /* Menu nav toggle */
+
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("nav").toggleClass("active");
+    })
+
+
+
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");    
+        $(blockId).slideToggle();
+
+    })
+
+
+    /* Slider */
+    $("[data-slider]").stick({
+        infinite: true,
+        fade: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+
     });
 
 
